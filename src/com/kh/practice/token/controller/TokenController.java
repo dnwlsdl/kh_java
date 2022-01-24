18d02b10bf5c534e93c1a1055af1765da859ef0f
JIN-DESKTOP\JIN <dnwls.KH@gmail.com>
2022-01-24 17:46:00 +0900

    나의 첫 프로젝트 커밋하기

---
+++ src/com/kh/practice/token/controller/TokenController.java
@@ -0,0 +1,19 @@
+package com.kh.practice.token.controller;
+
+import java.util.StringTokenizer;
+
+public class TokenController {
+	public TokenController {
+	}
+		public String afterToken(String str) {
+			str = StringTokenizer
+		}	
+			public String firstCap(String input) {
+			}
+				public void findChar() {
+				}	
+				
+			
+			
+		
+}
---
+++ src/com/kh/practice/token/view/TokenMenu.java
@@ -0,0 +1,20 @@
+package com.kh.practice.token.view;
+
+import java.util.Scanner;
+import com.kh.practice.token.controller.TokenController;
+
+public class TokenMenu {
+	private Scanner sc = new Scanner(System.in);
+	private TokenController tc = new TokenController();
+	
+	public void mainMenu() {
+		
+	}
+	public void tokenMenu() {
+		
+	}
+	public void inputMenu() {
+		
+	}
+
+}
